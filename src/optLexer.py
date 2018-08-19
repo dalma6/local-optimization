2deeb5b292ef976ae30eae5d622dc17ebd5bc4cb
Nikola Dimic <dimic.nikola@gmail.com>
2018-08-19 19:53:12 +0200

    lexer created

---
+++ src/optLexer.py
@@ -0,0 +1,75 @@
+import sys
+sys.path.insert(0, "./libraries")
+import lex as lex
+
+
+reserved = (
+    'IF',
+    'GOTO',
+    'IFFALSE',
+    'RETURN'
+)
+
+tokens = reserved + (
+   'NUMBER',
+   'ID',
+   'ASSIGN',
+   'LGT'
+) 
+
+literals = ['=', '+', '-', '*', '/', '(', ')']
+
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_ASSIGN(t):
+    r':='
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    if t.value in reserved:
+        t.type = t.value
+    return t
+
+def t_LGT(t):
+    r'(<=)|(>=)|[<>]'
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex(debug=0)
+
+test_data = '''
+a_1 := x + y 
+IFFALSE x GOTO 4
+b := 3 - y 
+c := 2 * y 
+IF c > 0 GOTO 9
+d := a + c
+'''
+
+lexer.input(test_data)
+
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break     
+    print(tok)
+
+
+
+
